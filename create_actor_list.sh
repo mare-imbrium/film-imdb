4eeb91daf6a4671482d9e4cd90a9f8d938b1479c
kepler <githubkepler.50s@gishpuppy.com>
2015-12-10 09:32:53 +0530

    added note about names with 2 cases

--- create_actor_list.sh
+++ create_actor_list.sh
@@ -13,7 +13,7 @@
 # 
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
-#          BUGS: ---
+#          BUGS: Single names with a (IVX) still contain that in the newname
 #         NOTES: ---
 #        AUTHOR: YOUR NAME (), 
 #  ORGANIZATION: 
@@ -46,6 +46,8 @@ fi
 #grep "^[^-$TAB]" $IFILE > t.t
 echo "Doubling the name field from $IFILE"
 grep "^[[:alpha:]]" $IFILE | awk -F$'\t' '{ print $1, $1 }' OFS=$'\t' > t.t
+#echo "Removing Molina, Óscar since he disallows a unique index collate nocase :("
+#grep -v "^Molina, Óscar" t.t | sponge t.t
 wc -l t.t
 echo Fixing the name and removing roman numbering in first column
 # there are some rows that fail this check since there is no comma in the name
@@ -55,6 +57,7 @@ echo Fixing the name and removing roman numbering in first column
 #gsed 's/^\([^,]*\), \([^(]*\)([IVX]*)/\2 \1/' t.t | tr -s ' ' > t.tt
 # second case takes care of names with no number
 gsed 's/^\([^,]*\), \([^(]*\)([IVX]*)/\2 \1/;s/^\([^,]*\), \([^(]*\)	/\2 \1	/' t.t | tr -s ' ' > t.tt
+
 wc -l t.tt
 echo Adding M and switching 1 and 2 cols
 awk -v a_gen=$gender -F$'\t' '{ print $2, $1, a_gen }' OFS=$'\t' t.tt > $gender.t
@@ -63,20 +66,19 @@ done
 echo sorting ...
 sort M.t F.t > names.tsv
 wc -l names.tsv
+# Some single names have that (IVX) in them, many Indian names.
+gsed 's/^\([^	]*	\)\([^	]*\) ([IVX]*)\(	.*\)/\1\2\3/' names.tsv | sponge names.tsv
 echo checking for double quotes in names
 grep -c '"' names.tsv
 echo escaping double quotes and sorting again
 sed 's/"/\\"/g' names.tsv | sort | sponge names.tsv
 wc -l names.tsv
-exit
-IFILE=actresses.topbilled
-if [[ ! -f "$IFILE" ]]; then
-    echo "File: $IFILE not found"
-    exit 1
-fi
-grep "^[^$TAB]" $IFILE > t.t
-gsed 's/^\([^,]*\), \([^(]*\)\(.*\)/\1, \2\3 	 \2 \1	F/' t.t | tr -s ' ' >> t.tt
-wc -l t.tt
-# sort unique since some names are duplicated in the actor and actress file
-sort -u t.tt > names.tsv
-
+echo WARNING There are about 62 cases of names that are present two times with a different case
+echo Usually in names with a middle name. e.g. Da Silva, Eric
+look 'Da Silva, Eric' names.tsv
+look 'da Silva, Eric' names.tsv
+echo Checking for duplicate names ....
+tr '[:upper:]' '[:lower:]' names.tsv  | sort -u > t.tu
+tr '[:upper:]' '[:lower:]' names.tsv  | sort > t.t
+comm -3 t.tu t.t  > duplicate_names.txt
+wc -l duplicate_names.txt
