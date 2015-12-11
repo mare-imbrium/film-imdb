01af44e4427e3792ead09b65e037d7b24a6bbf14
kepler <githubkepler.50s@gishpuppy.com>
2015-12-11 15:48:53 +0530

    allow passing of ending regex

--- readbyte.rb
+++ readbyte.rb
@@ -1,23 +1,26 @@
 #!/usr/bin/env ruby -w
 # ----------------------------------------------------------------------------- #
-#         File: ,,F
-#  Description: 
-#       Author:  r kumar
-#         Date: ,,D
-#  Last update: 2015-11-20 20:39
+#         File: readbyte.rb
+#  Description: reads the given file from the given offset, to the first blank line.
+#   The third argument is the regex to read until.
+#   In the case of the plot file it is "^-------".
+#       Author:  jkepler
+#         Date: 2015-12-01 or so
+#  Last update: 2015-12-11 12:39
 #      License: MIT License
 # ----------------------------------------------------------------------------- #
 
 
 filename=ARGV[0];
 offset=ARGV[1].to_i;
-#till=ARGV[2]
+till=ARGV[2] || "^$"
+reg = Regexp.new till
 File.open(filename) do |f|
     f.seek(offset, IO::SEEK_SET)
       #p f.read(10)
     while (line = f.gets)
       line = line.chomp
-      break if line =~ /^$/
+      break if line =~ reg
       puts line
     end
 end
